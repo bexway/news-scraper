6443c46b3b2747931b0f0ce90b0d5aba4d5e44f9
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-09-22 00:24:17 -0500

    master - comments written to html dom

--- public/comments.js
+++ public/comments.js
@@ -7,7 +7,12 @@ $(document).ready(function(){
         console.log(data)
         ////TODO: better error handling in for loop
         for (var i = 0; i < data.length; i++) {
-            console.log(data[i])
+            var comment = $('<div>').addClass("comment").attr("comment-id", data[i]._id);
+            comment.append($("<p>").addClass("comment-name").text(data[i].user));
+            comment.append($("<a>").addClass("comment-article").attr("href", data[i].article.url).text(data[i].article.headline));
+            comment.append($("<p>").addClass("comment-text").text(data[i].textContent));
+
+            $("#comments").append(comment);
         }
     });
 
