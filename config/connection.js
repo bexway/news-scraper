1127ca3e06fb23d6e8d8e597d01652d339c0da7e
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-09-23 17:42:33 -0500

    master - testing try-catch in connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,17 @@
 var mongoose = require('mongoose');
 
-// try {
-var connection = mongoose.connect(process.env.MONGODB_URI, {
-  useMongoClient: true,
-  /* other options */
-})
-// }
-// catch (e) {
-//   var connection = mongoose.connect('mongodb://localhost/newsscraper', {
-//     useMongoClient: true,
-//     /* other options */
-//   })
-// }
+try {
+  var connection = mongoose.connect(process.env.MONGODB_URI, {
+    useMongoClient: true,
+    /* other options */
+  })
+}
+catch (e) {
+  // connect locally if heroku doesn't work
+  var connection = mongoose.connect('mongodb://localhost/newsscraper', {
+    useMongoClient: true,
+    /* other options */
+  })
+}
 
 module.exports = connection;
