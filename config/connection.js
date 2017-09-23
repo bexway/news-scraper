3adfc294dfaabc2e2fbcbcdc1dc832f2b7e26074
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-09-23 17:35:48 -0500

    master - testing heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 var mongoose = require('mongoose');
 
-try {
-  var connection = mongoose.connect(MONGODB_URI, {
-    useMongoClient: true,
-    /* other options */
-  })
-}
-catch (e) {
-  var connection = mongoose.connect('mongodb://localhost/newsscraper', {
-    useMongoClient: true,
-    /* other options */
-  })
-}
+// try {
+var connection = mongoose.connect(process.env.MONGODB_URI, {
+  useMongoClient: true,
+  /* other options */
+})
+// }
+// catch (e) {
+//   var connection = mongoose.connect('mongodb://localhost/newsscraper', {
+//     useMongoClient: true,
+//     /* other options */
+//   })
+// }
 
 module.exports = connection;
