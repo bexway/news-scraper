21a073a2e8266ff457a6670d0282486ed594b006
Bex Way <rebeccaway2016@u.northwestern.edu>
2017-10-08 23:36:18 -0500

    master - basic styling on comments page

--- public/comments.js
+++ public/comments.js
@@ -5,9 +5,10 @@ $(document).ready(function(){
     $.getJSON("/api/comments", function(data) {
         for (var i = 0; i < data.length; i++) {
             var comment = $('<div>').addClass("comment").attr("comment-id", data[i]._id);
-            comment.append($("<p>").addClass("comment-name").text(data[i].user));
-            comment.append($("<a>").addClass("comment-article").attr("href", data[i].article.url).text(data[i].article.headline));
+            comment.append($("<p>").addClass("comment-name").text(data[i].user+" said: "));
             comment.append($("<p>").addClass("comment-text").text(data[i].textContent));
+            comment.append($("<a>").addClass("comment-article").attr("href", data[i].article.url).text("...about " + data[i].article.headline));
+            
 
             $("#comments").append(comment);
         }
